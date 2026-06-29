7a2125f8d87ae71e672aefb436ebeeab3d584f4a
aryanmishrahps-beep <aryanmishrahps@gmail.com>
2026-06-29 10:04:21 +0530

    Add files via upload

---
+++ script - Copy.js
@@ -0,0 +1,55 @@
+// Typing Animation
+
+const texts = [
+    "Frontend Developer",
+    "Web Designer",
+    "JavaScript Developer",
+    "Problem Solver"
+    ];
+    
+    let count = 0;
+    let index = 0;
+    let currentText = "";
+    let letter = "";
+    
+    (function type(){
+    
+    if(count === texts.length){
+    count = 0;
+    }
+    
+    currentText = texts[count];
+    
+    letter =
+    currentText.slice(0,++index);
+    
+    document.getElementById("typing")
+    .textContent = letter;
+    
+    if(letter.length === currentText.length){
+    
+    count++;
+    
+    index = 0;
+    
+    setTimeout(type,1000);
+    
+    }else{
+    
+    setTimeout(type,120);
+    
+    }
+    
+    })();
+    
+    
+    // Dark Mode
+    
+    document
+    .getElementById("themeBtn")
+    .addEventListener("click",()=>{
+    
+    document.body
+    .classList.toggle("dark");
+    
+    });
